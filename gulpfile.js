f99fee294ee5ae87e96c70718250c4b86a101505
Renan Couto <renancouto@gmail.com>
2016-05-19 07:00:01 -0300

    Update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 // dependencies
+var browserify = require('browserify');
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
 var nodemon = require('gulp-nodemon');
+var postcss = require('gulp-postcss');
+var source = require('vinyl-source-stream');
 var stylus = require('gulp-stylus');
+var watchify = require('watchify');
 
 // settings
 var paths = {
-  style: {
+  css: {
     main: './src/css/site.styl',
     all: './src/css/**/*.styl',
     output: './public/css/'
+  },
+
+  js: {
+    main: './src/js/site.js',
+    all: './src/js/**/*.js',
+    output: './public/js/'
   },
 
   html: {
@@ -20,16 +29,41 @@ var paths = {
 
 // gulp stylus
 gulp.task('stylus', function () {
-  return gulp.src(paths.style.main)
+  return gulp.src(paths.css.main)
     .pipe(stylus({ 'include css': true }))
     .pipe(postcss([ require('autoprefixer') ]))
-    .pipe(gulp.dest(paths.style.output))
+    .pipe(gulp.dest(paths.css.output))
     .pipe(browserSync.reload({ stream: true }));
+});
+
+// gulp browserify
+watchify.args.debug = true;
+var bundler = watchify(browserify(paths.js.main, watchify.args));
+
+function bundle () {
+  return bundler.bundle()
+    .on('error', function (err) {
+        if (err) console.log(err);
+        browserSync.notify('Browserify Error!');
+        this.emit('end');
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(paths.js.output))
+    // .pipe(browserSync.stream({ once: true }));
+}
+
+bundler.on('update', bundle);
+gulp.task('browserify', function () {
+  return bundle();
 });
 
 // gulp watchers
 gulp.task('watch:stylus', function () {
-  return gulp.watch(paths.style.all, [ 'stylus' ]);
+  return gulp.watch(paths.css.all, [ 'stylus' ]);
+});
+
+gulp.task('watch:browserify', function () {
+  return gulp.watch(paths.js.all, [ 'browserify' ]);
 });
 
 gulp.task('watch:jade', function () {
@@ -57,6 +91,6 @@ gulp.task('nodemon', function (next) {
 });
 
 // gulp tasks
-gulp.task('build', [ 'stylus' ]);
-gulp.task('watch', [ 'watch:stylus', 'watch:jade' ]);
+gulp.task('build', [ 'stylus', 'browserify' ]);
+gulp.task('watch', [ 'watch:stylus', 'watch:browserify', 'watch:jade' ]);
 gulp.task('default', [ 'build', 'watch', 'browser-sync' ]);
